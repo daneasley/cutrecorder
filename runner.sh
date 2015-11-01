567b549179ed49f6850c890ebcac99a9b5f813f5
Dan Easley <easleydr@jmu.edu>
2015-11-01 10:37:40 -0500

    Archived copy.

--- runner.sh
+++ runner.sh
@@ -3,28 +3,37 @@
 # Cut Recorder wrapper script
 
 # stupid hack to make sure we're in the working directory.  autostart file should do this but doesn't.
-
-cd /home/dan/cutrecorder
+    cd ~/cutrecorder
 
 # wait 15 seconds for system to finish booting
+    echo Waiting 15 seconds.
+    sleep 15s
 
-sleep 15s
+# load Meter,wait a bit for that to happen, connect it to system inputs, set size and position of window
+    echo Starting Meter System.
+    jalv.gtk http://gareus.org/oss/lv2/meters#dBTPmono &
+    sleep 10s
+    jack_connect system:capture_1 "True-Peak Meter (Mono):in"
+    jack_connect system:capture_2 "True-Peak Meter (Mono):in"
 
-# load Meter, connect to system input, set size and position of window
+# Logging
+    echo Configuring Logging System.
+    # delete log file
+        rm logfile.txt
+    # set log file variable                                             ***********************************
+        export ECASOUND_LOGFILE=~/cutrecorder/cutrecorder.log
+    # run following code in separate (minimized) terminal window                fix these!!!
+        #watch -n 1 -d tail --lines 40 ~/cutrecorder.log                ***********************************
 
-jalv.gtk http://gareus.org/oss/lv2/meters#dBTPmono &
+# Run Cut Recorder in infinite loop (performing kludgy cleanup on other applications' window size/locations each iteration)
+    echo Beginning recorder loop.
+    while true; do
+	    wmctrl -r Iceweasel -e 0,1281,0,1278,1000
+    	xdotool search --name "Meter" windowsize 78 980 windowmove 1190 24
+    	jack_connect system:capture_1 "True-Peak Meter (Mono):in"
+    	jack_connect system:capture_2 "True-Peak Meter (Mono):in"
+    	./cutrecorder.py
+    done
 
-sleep 10s
-jack_connect system:capture_1 "True-Peak Meter (Mono):in"
-jack_connect system:capture_2 "True-Peak Meter (Mono):in"
-
-# Run Cut Recorder in infinite loop
-
-while true; do
-	wmctrl -r Iceweasel -e 0,1281,0,1278,1000
-	xdotool search --name "Meter" windowsize 78 980 windowmove 1190 24
-	jack_connect system:capture_1 "True-Peak Meter (Mono):in"
-	jack_connect system:capture_2 "True-Peak Meter (Mono):in"
-	./cutrecorder.py
-done
+echo Terminating.
 
